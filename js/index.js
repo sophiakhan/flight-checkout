41440624dfbabfe02900d77c13d684d822c6e125
Sophia Khan <sophiaxkhan@gmail.com>
2017-03-21 17:04:55 -0400

    Created flight booking info

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
     
-}
+    $('form').css({color: 'white'});
+
+    $(".date").flatpickr();
+
+
+//     $('input').iCheck({
+//     checkboxClass: 'icheckbox_flat',
+//   });
+
+
+});
+
