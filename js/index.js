e52019b271c74b7679b697a07692c575a419e82c
Sophia Khan <sophiaxkhan@gmail.com>
2017-03-21 21:02:20 -0400

    Added planet animation

--- js/index.js
+++ js/index.js
@@ -15,6 +15,19 @@ $(document).ready(function() {
         fitContainer: true,
     });
 
+    $(".planet").planetarium({
+   autospin: "5000ms",
+   angle: "20deg",
+   glow: "rgba(255, 255, 255, 0.34902) 0px 0px 50px, inset 33px 20px 50px rgba(0,0,0,0.5)",
+   pattern: "./img/texture-saturn.jpg",
+   size: "100x100",
+   float: true,   
+   space: "body", 
+   ring: true, 
+   ringColor: "#fff", 
+   ringAngle: "20deg" 
+ });
+
     $('#modalButton').on('click', function() {
         $('#modal').removeClass('hidden');
     });
